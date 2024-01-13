baac11278fa27cd5ebbff0f4275be3c459afed5b
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-01-13 13:22:28 -0800

    Sets up Single Page Application page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import { Component } from 'react';
 import logo from './logo.svg';
+import CardList from './/components/card-list/card-list.jsx'
+import SearchBox from './/components/search-box/search-box.jsx'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+  // when do I get the list? The moment it's rendered by React
+  // how do I get the list? LifeCycle Methods to update and re-render
+  // where do I put the list? in State
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then((users)=> this.setState(() => {
+      return {monsters: users}
+    }
+    ));
+  }
+  
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(
+      () => {
+        return {searchField};
+      });
+  }
+
+
+  render() {
+    const {monsters, searchField } = this.state;
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    });
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox className='monsters-search-box' onChangeHandler= {onSearchChange} placeholder={"search monsters"} />
+      <CardList monsters = {filteredMonsters}/>
     </div>
   );
+}
 }
 
 export default App;
