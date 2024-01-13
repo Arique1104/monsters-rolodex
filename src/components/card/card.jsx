0db85d0d9d406dfb22ea66de3f80ba2f73b77ab6
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-01-13 13:23:30 -0800

    Adds correct card component and style

---
+++ src/components/card/card.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import './/card.styles.css';
+
+class Card extends Component {
+    render() {
+console.log(this.props)
+console.log("render from Card")
+// console.log(this.props.id)
+const {id, name, email} = this.props.monster;
+        // consts for props go here
+        // so do console.logs()
+        return (
+// this is where the div requirement goes for this card component
+            <div
+                key={id}
+                class='card-container'>
+                    
+                <img
+                    src={`https://robohash.org/${id}?set=set2&size=180x180`}
+                    alt={`monster ${name}`} />
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        )
+    }
+}
+
+export default Card;
