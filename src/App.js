b07b2f3b26cf54dfa712770f2503744b9c6f67e7
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2024-01-16 10:26:47 -0800

    Refactored from class component to functional component - plus search box experimentation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,46 @@
-import { Component } from 'react';
-import logo from './logo.svg';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './/components/card-list/card-list.jsx'
 import SearchBox from './/components/search-box/search-box.jsx'
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
-  // when do I get the list? The moment it's rendered by React
-  // how do I get the list? LifeCycle Methods to update and re-render
-  // where do I put the list? in State
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  console.log(searchField)
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+  const [stringField, setStringField] = useState('');
   
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response)=>response.json())
-    .then((users)=> this.setState(() => {
-      return {monsters: users}
-    }
-    ));
-  }
+      .then((response)=>response.json())
+      .then((users)=> setMonsters(users));
+  }, []);
   
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(
-      () => {
-        return {searchField};
-      });
+  useEffect(() => {
+    const newFilteredMonsters =  monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    }) 
+      setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+
+  const onSearchChange = (event) => {
+      const searchFieldString = event.target.value.toLocaleLowerCase();
+      setSearchField(searchFieldString);
+    }
+
+  const onStringChange = (event) => {
+    setStringField(event.target.value);
   }
 
-
-  render() {
-    const {monsters, searchField } = this.state;
-    const {onSearchChange} = this;
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField)
-    });
-
-    return (
-      <div className="App">
-        <h1 className='app-title'>Monsters Rolodex</h1>
-      <SearchBox className='monsters-search-box' onChangeHandler= {onSearchChange} placeholder={"search monsters"} />
-      <CardList monsters = {filteredMonsters}/>
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox className='monsters-search-box' onChangeHandler={onSearchChange} placeholder={"search monsters"} />
+      <SearchBox onChangeHandler={onStringChange} placeholder={"set string"} />
+       <CardList monsters={filteredMonsters} />
     </div>
-  );
-}
+  )
 }
 
 export default App;
